facf217851c2dd33316e27f3119e56e1c5ce7b68
Tim Keeley <tkeeley@hotmail.com>
2017-03-31 21:46:47 -0400

    Update replaceImage.js

--- replaceImage.js
+++ replaceImage.js
@@ -1,4 +1,4 @@
-Replaces all images on a page 
+//Replaces all images on a page 
 
 Array.prototype.slice.call(document.querySelectorAll('img')).map(function(el){
 	el.src = 'https://www.link-to-your-image.png';
